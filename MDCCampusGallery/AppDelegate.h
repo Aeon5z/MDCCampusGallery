72e1236575923b5f6329c1bacb7c5ec9ce2b43eb
Aeon5z <32336091+Aeon5z@users.noreply.github.com>
2017-10-14 21:08:42 -0700

    Initial Commit

---
+++ MDCCampusGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MDCCampusGallery
+//
+//  Created by Aeonz on 10/14/17.
+//  Copyright © 2017 Aeonz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MDCCampusGallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MDCCampusGallery
+//
+//  Created by Aeonz on 10/14/17.
+//  Copyright © 2017 Aeonz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
